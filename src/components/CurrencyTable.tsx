5db65a13369d248c841fe953b2448d412b9617c3
irines <irina.kiselyowa12@gmail.com>
2023-02-16 19:50:56 +0200

    MU input

--- src/components/CurrencyTable.tsx
+++ src/components/CurrencyTable.tsx
@@ -1,5 +1,6 @@
 import "../styles/currency-table.sass"
 import {ReactComponent as EditIcon} from "../assets/images/edit.svg";
+import { TextField } from "@mui/material";
 
 function CurrencyTable() {
     return (  
@@ -17,13 +18,30 @@ function CurrencyTable() {
                     <div className="cell currency-col"><div>EUR/UAH</div></div>
                     <div className="cell">
                         <div className="cell__content">
-                            <p>27.5</p>
+                            {/* <p>27.5</p> */}
+                            <TextField
+                                fullWidth 
+                                id="outlined-name"
+                                // label="Name"
+                                value={27.7}
+                                // onChange={handleChange}
+                            />
                             <EditIcon className="icon"/>
                         </div>
                     </div>
                     <div className="cell">
                         <div className="cell__content">
-                            <p>27.7</p>
+                            <TextField
+                                fullWidth 
+                                id="outlined-name"
+                                // label="Name"
+                                value={27.7}
+                                // onChange={handleChange}
+                                sx={{border: 'none', "& fieldset": { border: 'none' }, }}
+                                // InputProps={{
+                                //         disableUnderline: true,
+                                // }}
+                            />
                             <EditIcon className="icon"/>
                         </div>
                     </div>
