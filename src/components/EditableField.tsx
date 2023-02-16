9f7e6db8e692ea67195d6db825c27401119ba718
irines <irina.kiselyowa12@gmail.com>
2023-02-17 01:07:58 +0200

    Controlled field component with icons&validation

--- src/components/EditableField.tsx
+++ src/components/EditableField.tsx
@@ -1,49 +1,108 @@
-// import { TextField } from "@mui/material";
-// function EditableField() {
-//     return (  
-//         <>
-//             <TextField
-//                 fullWidth 
-//                 id="outlined-name"
-//                 // label="Name"
-//                 autoComplete='off'
-//                 value={fieldValue}
-//                 onChange={e => setFieldValue(e.target.value)}
-//                 onClick={e => setShowIcons(true)}
-//                 onMouseEnter={e => setShowEdit(!showIcons)}
-//                 onMouseLeave={e => setShowEdit(false)}
-//                 sx={{
-//                     borderColor: 'white', 
-//                     "& fieldset": { borderColor: 'white' }, 
-//                     "& .MuiOutlinedInput-root:hover": {
-//                         "& > fieldset": {
-//                         borderColor: 'white'
-//                         }
-//                     },
-//                     "& .MuiOutlinedInput-root.Mui-focused": {
-//                         "& > fieldset": {
-//                         borderColor: "#1976d2"
-//                     }
-//                 } 
-//                 }}
-//             />
-//             {
-//                 showEdit ?
-//                     <EditIcon className="icon"/>
-//                 : 
-//                 <></>
-//             }
-//             {
-//                 showIcons ?
-//                 <div className="helpIcons">
-//                     <CheckCircleIcon sx={{ fill: '#1976d2'}}/>
-//                     <CancelIcon sx={{ fill: '#1976d2'}}/>
-//                 </div>
-//                 :
-//                 <></>
-//             }
-//         </>
-//     );
-// }
+import { TextField } from "@mui/material";
+import {ReactComponent as EditIcon} from "../assets/images/edit.svg";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { useState, useEffect, ChangeEvent } from "react";
+import "../styles/currency-table.sass"
 
-// export default EditableField;
+interface FieldProps {
+    value: string
+}
+
+function EditableField({value} : FieldProps) {
+        const [initialValue, setInitialValue] = useState(value)
+        const [fieldValue, setFieldValue] = useState(value)
+        const [showIcons, setShowIcons] = useState(false)
+        const [showEdit, setShowEdit] = useState(false)
+        const [valid, setValid] = useState(true)
+
+        useEffect(() => {
+            setFieldValue(value);
+            setInitialValue(value);
+        }, [value]);
+
+        const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+            console.log("handelChange")
+            const newVal = (e.target.value)
+            if (Number(newVal) < Number(initialValue)*1.1 && Number(newVal) > Number(initialValue) * 0.9 ) {
+                setValid(true)
+            } else {
+                setValid(false)
+            }
+            setFieldValue(newVal)
+        }
+
+        const handleReset = () => {
+            setFieldValue(initialValue)
+            setValid(true)
+            setShowIcons(false)
+            setShowEdit(false)
+        }
+
+        const handleSubmit = () => {
+            // request to server
+            setShowIcons(false)
+            setShowEdit(false)
+        }
+
+    return (  
+        <>
+            {
+                valid
+                ?
+                <TextField
+                    fullWidth 
+                    id="outlined-name"
+                    // label="Name"
+                    autoComplete='off'
+                    value={fieldValue}
+                    onChange={handleChange}
+                    onClick={e => setShowIcons(true)}
+                    onMouseEnter={e => setShowEdit(!showIcons)}
+                    onMouseLeave={e => setShowEdit(false)}
+                    sx={{
+                        borderColor: 'white', 
+                        "& fieldset": { borderColor: 'white' }, 
+                        "& .MuiOutlinedInput-root:hover": {
+                            "& > fieldset": {
+                            borderColor: 'white'
+                            }
+                        },
+                        "& .MuiOutlinedInput-root.Mui-focused": {
+                            "& > fieldset": {
+                            borderColor: "#1976d2"
+                        }
+                    } 
+                    }}
+                />
+                :
+                <TextField
+                    error
+                    id="outlined-error-helper-text"
+                    label="Must be +- 10%."
+                    value={fieldValue}
+                    onChange={handleChange}
+                    // helperText="Must be +- 10%."
+              />
+            }
+            
+            {
+                showEdit ?
+                    <EditIcon className="icon"/>
+                : 
+                <></>
+            }
+            {
+                showIcons ?
+                <div className="helpIcons">
+                    {valid ? <div onClick={handleSubmit}><CheckCircleIcon sx={{ fill: '#1976d2'}}/></div> : <CheckCircleIcon sx={{ fill: '#b5b5b5'}}/>}
+                    <div onClick={handleReset}><CancelIcon sx={{ fill: '#1976d2'}}/></div>
+                </div>
+                :
+                <></>
+            }
+        </>
+    );
+}
+
+export default EditableField;
--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -4,6 +4,7 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import CancelIcon from '@mui/icons-material/Cancel';
 import {useState, useEffect} from 'react';
 import "../styles/currency-table.sass"
+import EditableField from "./EditableField";
 
 interface RowData {
     value: {ccy: string, base_ccy:string, buy:string, sale:string};
@@ -11,107 +12,24 @@ interface RowData {
 }
 
 function TableRow({value, key}:RowData) {
-    const [fieldValueBuy, setFieldValueBuy] = useState(value.buy);
-    const [fieldValueSell, setFieldValueSell] = useState(value.sale);
-    const [showIconsBuy, setShowIconsBuy] = useState(false)
-    const [showIconsSell, setShowIconsSell] = useState(false)
-    const [showEditBuy, setShowEditBuy] = useState(false)
-    const [showEditSell, setShowEditSell] = useState(false)
-
-    useEffect(() => {
-        setFieldValueBuy(value.buy);
-        setFieldValueSell(value.sale);
-    }, [value]);
 
     return (  
         <div className="row" key={key}>
             <div className="cell currency-col"><div>{`${value.ccy}/${value.base_ccy}`}</div></div>
                 <div className="cell">
                     <div className="cell__content">
-                        <TextField
-                            fullWidth 
-                            id="outlined-name"
-                            // label="Name"
-                            autoComplete='off'
-                            value={fieldValueBuy}
-                            onChange={e => setFieldValueBuy(e.target.value)}
-                            onClick={e => setShowIconsBuy(true)}
-                            onMouseEnter={e => setShowEditBuy(!showIconsBuy)}
-                            onMouseLeave={e => setShowEditBuy(false)}
-                            sx={{
-                                borderColor: 'white', 
-                                "& fieldset": { borderColor: 'white' }, 
-                                "& .MuiOutlinedInput-root:hover": {
-                                    "& > fieldset": {
-                                    borderColor: 'white'
-                                    }
-                                },
-                                "& .MuiOutlinedInput-root.Mui-focused": {
-                                    "& > fieldset": {
-                                    borderColor: "#1976d2"
-                                }
-                            } 
-                            }}
+                        <EditableField 
+                            value={value.buy} 
+                            // setFieldValue={setFieldValueBuy}
                         />
-                        {
-                            showEditBuy ?
-                                <EditIcon className="icon"/>
-                            : 
-                            <></>
-                        }
-                        {
-                            showIconsBuy ?
-                            <div className="helpIcons">
-                                <CheckCircleIcon sx={{ fill: '#1976d2'}}/>
-                                <CancelIcon sx={{ fill: '#1976d2'}}/>
-                            </div>
-                            :
-                            <></>
-                        }
                     </div>
                 </div>
                 <div className="cell">
                     <div className="cell__content">
-                        <TextField
-                            fullWidth 
-                            id="outlined-name"
-                            // label="Name"
-                            autoComplete='off'
-                            value={fieldValueSell}
-                            onChange={e => setFieldValueSell(e.target.value)}
-                            onClick={e => setShowIconsSell(true)}
-                            onMouseEnter={e => setShowEditSell(!showIconsSell)}
-                            onMouseLeave={e => setShowEditSell(false)}
-                            sx={{
-                                borderColor: 'white', 
-                                "& fieldset": { borderColor: 'white' }, 
-                                "& .MuiOutlinedInput-root:hover": {
-                                    "& > fieldset": {
-                                    borderColor: 'white'
-                                    }
-                                },
-                                "& .MuiOutlinedInput-root.Mui-focused": {
-                                    "& > fieldset": {
-                                    borderColor: "#1976d2"
-                                }
-                            } 
-                            }}
+                        <EditableField 
+                            value={value.sale}
+                            // setFieldValue={setFieldValueSell}
                         />
-                        {
-                            showEditSell ?
-                                <EditIcon className="icon"/>
-                            : 
-                            <></>
-                        }
-                        {
-                            showIconsSell ?
-                            <div className="helpIcons">
-                                <CheckCircleIcon/>
-                                <CancelIcon/>
-                            </div>
-                            :
-                            <></>
-                        }
                 </div>
             </div>
         </div> 
